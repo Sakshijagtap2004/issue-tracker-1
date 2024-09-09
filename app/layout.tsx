82664505f92cc3f0202b6343fe9fd1bcc41cd034
Sakshijagtap-27 <21104082.sakshi.jagtap@gmail.com>
2024-09-09 13:51:08 +0530

    styled the active link in navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NavBar/>
+        
         {children}
       </body>
     </html>
